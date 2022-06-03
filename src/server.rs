0eb26cc682450e3b9d71e482febd41fcd8bccf55
jsgthb <jeff.schaack@gmail.com>
2022-06-03 17:45:05 +0200

    Added pretty print for filesizes

--- src/server.rs
+++ src/server.rs
@@ -7,23 +7,35 @@ struct File {
 }
 
 fn main() {
-    let mut fileArray: Vec<File> = Vec::new();
+    let mut file_array: Vec<File> = Vec::new();
     let mut counter = 0;
     println!("Checking files on server (/files/server)...");
     // Loop through all files in server directory
     for file in fs::read_dir("./files/server").unwrap() {
         // Store file data
-        let fileName = file.as_ref().unwrap().file_name().into_string().unwrap();
-        let filePath = file.as_ref().unwrap().path().display().to_string();
-        let fileLength = file.unwrap().metadata().unwrap().len();
-        fileArray.push(File { name: fileName.clone(), path: filePath.clone(), size: fileLength.clone() });
+        let file_name = file.as_ref().unwrap().file_name().into_string().unwrap();
+        let file_path = file.as_ref().unwrap().path().display().to_string();
+        let file_length = file.unwrap().metadata().unwrap().len();
+        file_array.push(File { name: file_name.clone(), path: file_path.clone(), size: file_length.clone() });
         // Display file data
-        println!("{}: {} {}", counter, fileName, fileLength);
+        println!("{}: '{}' {}", counter, file_name, pretty_print_filesize(file_length));
         counter += 1;
     }
     // End process if no files are located in server folder
     if counter == 0 {
         println!("No files found, shutting down");
         return;
+    }
+}
+
+fn pretty_print_filesize(length: u64) -> String {
+    if length < 1024 {
+        return format!("{} Bytes", length);
+    } else if length >= 1024 && length < 1024 * 1024 {
+        let output_length: f64 = length as f64 / 1024 as f64;
+        return format!("{} kB", output_length);
+    } else {
+        let output_length: f64 = length as f64 / 1024 as f64 / 1024 as f64;
+        return format!("{:.2} MB", output_length);
     }
 }
